7243a79158e666b4bc8a914fdc48b08357de7902
Chris Sachs <c9r@me.com>
2012-10-06 20:15:00 -0700

    Back to basics.

--- latest/api/lib/template.js
+++ latest/api/lib/template.js
@@ -2,13 +2,29 @@
 // code by Gilles Dubochet with contributions by Pedro Furlanetto
 
 $(document).ready(function(){
+
+    // Escapes special characters and returns a valid jQuery selector
+    function escapeJquery(str){
+        return str.replace(/([;&,\.\+\*\~':"\!\^#$%@\[\]\(\)=>\|])/g, '\\$1');
+    }
+
+    // highlight and jump to selected member
+    if (window.location.hash) {
+      var temp = window.location.hash.replace('#', '');
+      var elem = '#'+escapeJquery(temp);
+
+      window.scrollTo(0, 0);
+      $(elem).parent().effect("highlight", {color: "#FFCC85"}, 3000);
+      $('html,body').animate({scrollTop:$(elem).parent().offset().top}, 1000);
+    }
+    
     var isHiddenClass = function (name) {
         return name == 'scala.Any' ||
-               name == 'scala.AnyRef' ||
-               name == 'scala.Predef.any2stringfmt' ||
-               name == 'scala.Predef.any2stringadd' ||
-               name == 'scala.Predef.any2ArrowAssoc' ||
-               name == 'scala.Predef.any2Ensuring'
+               name == 'scala.AnyRef';
+    };
+
+    var isHidden = function (elem) {
+        return $(elem).attr("data-hidden") == 'true';
     };
 
     $("#linearization li:gt(0)").filter(function(){
@@ -16,7 +32,7 @@ $(document).ready(function(){
     }).removeClass("in").addClass("out");
 
     $("#implicits li").filter(function(){
-        return isHiddenClass($(this).attr("name"));
+        return isHidden(this);
     }).removeClass("in").addClass("out");
 
     // Pre-filter members
@@ -42,20 +58,20 @@ $(document).ready(function(){
 
         case 33: //page up
             input.val("");
-            filter(false);            
+            filter(false);
             break;
 
         case 34: //page down
             input.val("");
-            filter(false);            
-            break;            
-
-        default: 
-            window.scrollTo(0, $("#mbrsel").offset().top);
-            filter(true);        
+            filter(false);
             break;
 
-        }        
+        default:
+            window.scrollTo(0, $("#mbrsel").offset().top);
+            filter(true);
+            break;
+
+        }
     });
     input.focus(function(event) {
         input.select();
@@ -69,7 +85,7 @@ $(document).ready(function(){
         if (event.keyCode == 9) { // tab
             $("#index-input", window.parent.document).focus();
             input.attr("value", "");
-            return false;            
+            return false;
         }
     });
 
@@ -101,6 +117,12 @@ $(document).ready(function(){
         $("#linearization li.in").removeClass("in").addClass("out");
         $("#linearization li:first").removeClass("out").addClass("in");
         $("#implicits li.in").removeClass("in").addClass("out");
+
+        if ($(this).hasClass("out") && $("#mbrsel > div[id=ancestors] > ol > li.showall").hasClass("in")) {
+            $(this).removeClass("out").addClass("in");
+            $("#mbrsel > div[id=ancestors] > ol > li.showall").removeClass("in").addClass("out");
+        }
+
         filter();
     })
     $("#mbrsel > div[id=ancestors] > ol > li.showall").click(function() {
@@ -112,9 +134,14 @@ $(document).ready(function(){
 
         var filteredImplicits =
         $("#implicits li.out").filter(function() {
-            return ! isHiddenClass($(this).attr("name"));
+            return ! isHidden(this);
         });
         filteredImplicits.removeClass("out").addClass("in");
+
+        if ($(this).hasClass("out") && $("#mbrsel > div[id=ancestors] > ol > li.hideall").hasClass("in")) {
+            $(this).removeClass("out").addClass("in");
+            $("#mbrsel > div[id=ancestors] > ol > li.hideall").removeClass("in").addClass("out");
+        }
 
         filter();
     });
@@ -134,18 +161,21 @@ $(document).ready(function(){
     });
     $("#order > ol > li.alpha").click(function() {
         if ($(this).hasClass("out")) {
-            $(this).removeClass("out").addClass("in");
-            $("#order > ol > li.inherit").removeClass("in").addClass("out");
             orderAlpha();
         };
     })
     $("#order > ol > li.inherit").click(function() {
         if ($(this).hasClass("out")) {
-            $(this).removeClass("out").addClass("in");
-            $("#order > ol > li.alpha").removeClass("in").addClass("out");
             orderInherit();
         };
     });
+    $("#order > ol > li.group").click(function() {
+        if ($(this).hasClass("out")) {
+            orderGroup();
+        };
+    });
+    $("#groupedMembers").hide();
+
     initInherit();
 
     // Create tooltips
@@ -184,29 +214,31 @@ $(document).ready(function(){
     });
 
     /* Linear super types and known subclasses */
-    function toggleShowContentFct(outerElement){
-      var content = $(".hiddenContent", outerElement);
-      var vis = $(":visible", content);
-      if (vis.length > 0) {
+    function toggleShowContentFct(e){
+      e.toggleClass("open");
+      var content = $(".hiddenContent", e.parent().get(0));
+      if (content.is(':visible')) {
         content.slideUp(100);
-        $(".showElement", outerElement).show();
-        $(".hideElement", outerElement).hide();
       }
       else {
         content.slideDown(100);
-        $(".showElement", outerElement).hide();
-        $(".hideElement", outerElement).show();
       }
     };
-    $(".toggleContainer").click(function() {
+
+    $(".toggle:not(.diagram-link)").click(function() {
       toggleShowContentFct($(this));
     });
 
     // Set parent window title
     windowTitle();
+
+    if ($("#order > ol > li.group").length == 1) { orderGroup(); };
 });
 
 function orderAlpha() {
+    $("#order > ol > li.alpha").removeClass("out").addClass("in");
+    $("#order > ol > li.inherit").removeClass("in").addClass("out");
+    $("#order > ol > li.group").removeClass("in").addClass("out");
     $("#template > div.parent").hide();
     $("#template > div.conversion").hide();
     $("#mbrsel > div[id=ancestors]").show();
@@ -214,9 +246,22 @@ function orderAlpha() {
 };
 
 function orderInherit() {
+    $("#order > ol > li.inherit").removeClass("out").addClass("in");
+    $("#order > ol > li.alpha").removeClass("in").addClass("out");
+    $("#order > ol > li.group").removeClass("in").addClass("out");
     $("#template > div.parent").show();
     $("#template > div.conversion").show();
     $("#mbrsel > div[id=ancestors]").hide();
+    filter();
+};
+
+function orderGroup() {
+    $("#order > ol > li.group").removeClass("out").addClass("in");
+    $("#order > ol > li.alpha").removeClass("in").addClass("out");
+    $("#order > ol > li.inherit").removeClass("in").addClass("out");
+    $("#template > div.parent").hide();
+    $("#template > div.conversion").hide();
+    $("#mbrsel > div[id=ancestors]").show();
     filter();
 };
 
@@ -227,44 +272,74 @@ function orderInherit() {
   *  - initialises a control variable used by the filter method to control whether filtering happens on flat members
   *    or on inheritance-grouped members. */
 function initInherit() {
-    // parents is a map from fully-qualified names to the DOM node of parent headings.
-    var parents = new Object();
+    // inheritParents is a map from fully-qualified names to the DOM node of parent headings.
+    var inheritParents = new Object();
+    var groupParents = new Object();
     $("#inheritedMembers > div.parent").each(function(){
-        parents[$(this).attr("name")] = $(this);
+        inheritParents[$(this).attr("name")] = $(this);
     });
     $("#inheritedMembers > div.conversion").each(function(){
-        parents[$(this).attr("name")] = $(this);
+        inheritParents[$(this).attr("name")] = $(this);
     });
+    $("#groupedMembers > div.group").each(function(){
+        groupParents[$(this).attr("name")] = $(this);
+    });
+
     $("#types > ol > li").each(function(){
         var mbr = $(this);
         this.mbrText = mbr.find("> .fullcomment .cmt").text();
         var qualName = mbr.attr("name");
         var owner = qualName.slice(0, qualName.indexOf("#"));
         var name = qualName.slice(qualName.indexOf("#") + 1);
-        var parent = parents[owner];
-        if (parent != undefined) {
-            var types = $("> .types > ol", parent);
+        var inheritParent = inheritParents[owner];
+        if (inheritParent != undefined) {
+            var types = $("> .types > ol", inheritParent);
             if (types.length == 0) {
-                parent.append("<div class='types members'><h3>Type Members</h3><ol></ol></div>");
-                types = $("> .types > ol", parent);
+                inheritParent.append("<div class='types members'><h3>Type Members</h3><ol></ol></div>");
+                types = $("> .types > ol", inheritParent);
+            }
+            var clone = mbr.clone();
+            clone[0].mbrText = this.mbrText;
+            types.append(clone);
+        }
+        var group = mbr.attr("group")
+        var groupParent = groupParents[group];
+        if (groupParent != undefined) {
+            var types = $("> .types > ol", groupParent);
+            if (types.length == 0) {
+                groupParent.append("<div class='types members'><ol></ol></div>");
+                types = $("> .types > ol", groupParent);
             }
             var clone = mbr.clone();
             clone[0].mbrText = this.mbrText;
             types.append(clone);
         }
     });
+
     $("#values > ol > li").each(function(){
         var mbr = $(this);
         this.mbrText = mbr.find("> .fullcomment .cmt").text();
         var qualName = mbr.attr("name");
         var owner = qualName.slice(0, qualName.indexOf("#"));
         var name = qualName.slice(qualName.indexOf("#") + 1);
-        var parent = parents[owner];
-        if (parent != undefined) {
-            var values = $("> .values > ol", parent);
+        var inheritParent = inheritParents[owner];
+        if (inheritParent != undefined) {
+            var values = $("> .values > ol", inheritParent);
             if (values.length == 0) {
-                parent.append("<div class='values members'><h3>Value Members</h3><ol></ol></div>");
-                values = $("> .values > ol", parent);
+                inheritParent.append("<div class='values members'><h3>Value Members</h3><ol></ol></div>");
+                values = $("> .values > ol", inheritParent);
+            }
+            var clone = mbr.clone();
+            clone[0].mbrText = this.mbrText;
+            values.append(clone);
+        }
+        var group = mbr.attr("group")
+        var groupParent = groupParents[group];
+        if (groupParent != undefined) {
+            var values = $("> .values > ol", groupParent);
+            if (values.length == 0) {
+                groupParent.append("<div class='values members'><ol></ol></div>");
+                values = $("> .values > ol", groupParent);
             }
             var clone = mbr.clone();
             clone[0].mbrText = this.mbrText;
@@ -277,6 +352,9 @@ function initInherit() {
     $("#inheritedMembers > div.conversion").each(function() {
         if ($("> div.members", this).length == 0) { $(this).remove(); };
     });
+    $("#groupedMembers > div.group").each(function() {
+        if ($("> div.members", this).length == 0) { $(this).remove(); };
+    });
 };
 
 /* filter used to take boolean scrollToMember */
@@ -286,26 +364,43 @@ function filter() {
     var queryRegExp = new RegExp(query, "i");
     var privateMembersHidden = $("#visbl > ol > li.public").hasClass("in");
     var orderingAlphabetic = $("#order > ol > li.alpha").hasClass("in");
-    var hiddenSuperclassElementsLinearization = orderingAlphabetic ? $("#linearization > li.out") : $("#linearization > li:gt(0)");
+    var orderingInheritance = $("#order > ol > li.inherit").hasClass("in");
+    var orderingGroups = $("#order > ol > li.group").hasClass("in");
+    var hiddenSuperclassElementsLinearization = orderingInheritance ? $("#linearization > li:gt(0)") : $("#linearization > li.out");
     var hiddenSuperclassesLinearization = hiddenSuperclassElementsLinearization.map(function() {
       return $(this).attr("name");
     }).get();
-    var hiddenSuperclassElementsImplicits = orderingAlphabetic ? $("#implicits > li.out") : $("#implicits > li");
+    var hiddenSuperclassElementsImplicits = orderingInheritance ? $("#implicits > li") : $("#implicits > li.out");
     var hiddenSuperclassesImplicits = hiddenSuperclassElementsImplicits.map(function() {
       return $(this).attr("name");
     }).get();
 
     var hideInheritedMembers;
 
-    if(orderingAlphabetic) {
+    if (orderingAlphabetic) {
+      $("#allMembers").show();
       $("#inheritedMembers").hide();
+      $("#groupedMembers").hide();
       hideInheritedMembers = true;
       $("#allMembers > .members").each(filterFunc);
-    }
-    else {
+    } else if (orderingGroups) {
+      $("#groupedMembers").show();
+      $("#inheritedMembers").hide();
+      $("#allMembers").hide();
+      hideInheritedMembers = true;
+      $("#groupedMembers  > .group > .members").each(filterFunc);
+      $("#groupedMembers  > div.group").each(function() {
+        $(this).show();
+        if ($("> div.members", this).not(":hidden").length == 0) {
+            $(this).hide();
+        } else {
+            $(this).show();
+        }
+      });
+    } else if (orderingInheritance) {
       $("#inheritedMembers").show();
-      hideInheritedMembers = true;
-      $("#allMembers > .members").each(filterFunc);
+      $("#groupedMembers").hide();
+      $("#allMembers").hide();
       hideInheritedMembers = false;
       $("#inheritedMembers > .parent > .members").each(filterFunc);
       $("#inheritedMembers > .conversion > .members").each(filterFunc);
